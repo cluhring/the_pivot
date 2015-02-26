cf3536bc48b3a4b4dc866d68f1b84e9f484c5d02
Bryce Holcomb <bryce_holcomb@outlook.com>
2015-02-26 11:26:41 -0700

    test coverage is better now

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,26 +1,2 @@
 class AdminsController < Admin::BaseController
-  def show
-    @admin = Admin.find(params[:id])
-    authorize! :read, @admin
-  end
-
-  def new
-    @admin = Admin.new
-    authorize! :new, @admin
-  end
-
-  def create
-    @admin = Admin.new(admin_params)
-    authorize! :create, @admin
-    if @admin.save
-      redirect_to root_path, notice: "Admin created successfully."
-    else
-      redirect_to new_admin_path, error: "Invalid  Credentials"
-    end
-  end
-
-  private
-  def admin_params
-    params.require(:admin).permit(:first_name, :last_name, :email, :password)
-  end
 end
